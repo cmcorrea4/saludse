6421a27a46b176b017f9b9c5ed7f87075889944f
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-11-06 16:53:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ TEMPC = st.number_input("Temperatura",key="1")
 TIMEC = st.number_input("Tiempo",key="2")
 if st.button("Preparar"):
     # Crear mensaje JSON
-    mensaje=f"{TEMPC},{TIMEC}" 
+    mensaje=f"{TEMPC} grados,{TIMEC} min" 
     send_mqtt_message(mensaje)
     client = mqtt.Client()
     client.connect(MQTT_BROKER, MQTT_PORT, 60)
